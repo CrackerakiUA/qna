de2b7d8a42e0b64ab4dfb3c55ac994cf79d2d4d2
Honchar Denys <crackeraki@gmail.com>
2021-07-13 11:32:55 +0300

    added store service

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class StoreService {
-
-  constructor() { }
+	constructor(){}
+	set(hold, value){
+		try { localStorage.setItem('temp_storage_'+hold, JSON.stringify(value)); }
+		catch(e){}
+	}
+	get(hold, cb:any=()=>{}, errcb:any=()=>{}){
+		let doc = localStorage.getItem('temp_storage_'+hold);
+		cb(doc&&JSON.parse(doc)||null);
+	}
+	remove(hold, cb:any=()=>{}, errcb:any=()=>{}){
+		localStorage.removeItem('temp_storage_'+hold);
+	}
+	clear(cb:any=()=>{}, errcb:any=()=>{}){
+		localStorage.clear();
+	}
 }
